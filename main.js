d493493b2b5fd2bc713785118a2ed66a79e5b03a
Juan Sierra <jksierra@gmail.com>
2019-09-14 09:15:05 +0100

    Succesful rendering

--- main.js
+++ main.js
@@ -3,13 +3,10 @@ const fs = require('fs');
 let data = fs.readFileSync('data/retrotop40.json');
 let entries = JSON.parse(data);
 
-const alex = entries.filter(e => e.Autor == "Alex");
-const pol = entries.filter(e => e.Autor == "Pol");
-const nico = entries.filter(e => e.Autor == "Nico");
+let filtered = {
+	"Alex" : entries.filter(e => e.Autor == "Alex"),
+	"Pol" : entries.filter(e => e.Autor == "Pol"),
+	"Nico" : entries.filter(e => e.Autor == "Nico")
+};
 
-let filtered = [];
-filtered.push(alex);
-filtered.push(pol);
-filtered.push(nico);
-
-let file = fs.writeFileSync('retrotop40.filtered.json', JSON.stringify(filtered));
+let file = fs.writeFileSync('data/retrotop40.filtered.json', JSON.stringify(filtered));
