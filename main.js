23b25d69e7f338a9a088247f16a59a3fada4532d
Juan Sierra <jksierra@gmail.com>
2019-10-16 07:17:55 +0100

    Best mobile appearance and fixed a couple of photos

--- main.js
+++ main.js
@@ -8,7 +8,8 @@ let entries = JSON.parse(data);
 let filtered = {
 	"Alex" : entries.filter(e => e.Autor == "Alex"),
 	"Pol" : entries.filter(e => e.Autor == "Pol"),
-	"Nico" : entries.filter(e => e.Autor == "Nico")
+	"Nico" : entries.filter(e => e.Autor == "Nico"),
+	"Todos" : entries.filter(e => e.Autor == "Todos")
 };
 
 var rendered = Mustache.render(template, filtered);
